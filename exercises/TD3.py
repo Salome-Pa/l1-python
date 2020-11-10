09edde052ff4915841e5f6e2c0d5d6ab42348e76
Salome <salome.pacelat@ens.uvsq.fr>
2020-11-10 14:59:31 +0100

    ko

--- exercises/TD3.py
+++ exercises/TD3.py
@@ -5,7 +5,7 @@ def demandeTemps():
     seconde = int(input("combien de sec"))
     if (seconde > 59 or minute > 59 or heure > 23):
         print("entree mal formee, ce n est pas une date")
-        return (0,0,0,0)
+        return (0, 0, 0, 0)
     return (jour,heure,minute,seconde)
 afficheTemps(demandeTemps())
 
@@ -13,8 +13,8 @@ def proportionTemps(temps,proportion):
     return secondeEnTemps(int(tempsEnSeconde(temps)*proportion))
 
     #tempsEnSeconde(temps)*proportion (0.2) --> nb sec (pas float mais int(nb seconde)
-     afficheTemps(proportionTemps((2,0,36,0),0.2))
-     afficheTemps(proportionTemps(proportion = 0.2, temps = (2,0,36,0)))
+     afficheTemps(proportionTemps((2, 0, 36, 0), 0.2))
+     afficheTemps(proportionTemps(proportion = 0.2, temps = (2, 0, 36 ,0)))
 
 
 
@@ -82,11 +82,11 @@ def verifie(liste_temps):
         return False
     temps_total = 0
     for elem in liste_temps:
-        if(tempsEnSeconde(elem) > tempsEnSeconde((0,48,0,0))):
+        if(tempsEnSeconde(elem) > tempsEnSeconde((0, 48, 0, 0)):
             return False 
         temps_total += tempsEnSeconde(elem)
-    return temps_total <= tempsEnSeconde(0,140,0,0)
+    return temps_total <= tempsEnSeconde(0, 140, 0, 0)
 
 
-liste_temps = [[1,2,39,34],[0,1,9,4],[0,29,39,51],[0,31,13,46]] 
+liste_temps = [[1, 2, 39, 34],[0, 1, 9 ,4],[0, 29, 39, 51],[0, 31, 13, 46]] 
 verifie(liste_temps)
