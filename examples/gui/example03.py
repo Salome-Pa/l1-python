270d67478b0766f86afb8fee3b0fca3308e8cf17
Salome <salome.pacelat@ens.uvsq.fr>
2020-11-29 16:09:58 +0100

    ll

--- examples/gui/example03.py
+++ examples/gui/example03.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
 
-
+if __name__ == '__main__':
     root = Tk()
 
     canvas = Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
