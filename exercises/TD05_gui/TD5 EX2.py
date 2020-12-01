c8003daa6198047a749736aeb3e6f73b1ef1b101
Salome <salome.pacelat@ens.uvsq.fr>
2020-12-01 15:42:26 +0100

    butrz

--- exercises/TD05_gui/TD5 EX2.py
+++ exercises/TD05_gui/TD5 EX2.py
@@ -4,27 +4,34 @@ import random
 racine = tk.Tk() # Création de la fenêtre racine
 racine.title("dessin") # titre
 
+couleur_base = "blue"
+def couleur():
+    global couleur_base
+    couleur_base = input("couleur")
+
 
 def fct_cercle():
     X= random.randint(0,500)
     Y= random.randint(0,500)
-    carre_noir.create_oval(X, Y, 100+X, 100+Y, outline = "blue")
+    carre_noir.create_oval(X, Y, 100+X, 100+Y, fill = couleur_base)
 def fct_carre():
     N= random.randint(0,500)
     V= random.randint(0,500)
-    carre_noir.create_rectangle(N, V, 100+N, 100+V, outline = "red", fill="red")
+    carre_noir.create_rectangle(N, V, 100+N, 100+V, outline = couleur_base, fill= couleur_base)
 def fct_croix():
     B= random.randint(0,500)
     D= random.randint(0,500)
-    carre_noir.create_rectangle(B, D, 100+B, 100+D, outline = "black")
-    carre_noir.create_line((B, D),(100+B, 100+D), fill= "yellow", width=3)
-    carre_noir.create_line((B+100, D),(B, D+100), fill= "yellow", width=3)
+    carre_noir.create_rectangle(B, D, 100+B, 100+D, outline = couleur_base)
+    carre_noir.create_line((B, D),(100+B, 100+D), fill= couleur_base, width=3)
+    carre_noir.create_line((B+100, D),(B, D+100), fill= couleur_base, width=3)
 
 carre_noir = tk.Canvas(racine, bg="black", height=600, width= 600) # création widget
-bouton1 = tk.Button(racine, text="couleur", font = ("helvetica", "10") )
+bouton1 = tk.Button(racine, text="couleur", font = ("helvetica", "10"), command= couleur)
 bouton2 = tk.Button(racine, text="cercle", font = ("helvetica", "10"), command = fct_cercle)
 bouton3 = tk.Button(racine, text="carre", font = ("helvetica", "10"), command = fct_carre)
 bouton4 = tk.Button(racine, text="croix", font = ("helvetica", "10"), command = fct_croix)
+
+
 
 
 bouton1.grid(column=1, row=0) # position widget
