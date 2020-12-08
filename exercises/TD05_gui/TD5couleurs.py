345f442eb2c2cb84aae88b46bef6eb065ae37805
Salome <salome.pacelat@ens.uvsq.fr>
2020-12-08 12:33:32 +0100

    fufvhikhjghft

--- exercises/TD05_gui/TD5couleurs.py
+++ exercises/TD05_gui/TD5couleurs.py
@@ -1,21 +1,40 @@
+r = 0
+g = 0
+b = 0
+r = int(input("valeur de r : "))
+g = int(input("valeur de g : "))
+b = int(input("valeur de b : "))
+
 def get_color(r, g, b):
     """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
+i = int(input("coordos i"))
+j = int(input("coordos j"))
+
 def draw_pixel(i, j, color):
     """pixel (i,j) color"""
-    carre_noir.create_line(i,j,i,j, fill = color)
+    carre_noir.create_rectangle(i,j,i - 1,j - 1, outline=color)
+
+import random
+
+def ecran_aleatoire():
+    for i in range(256):
+        for u in range(256):
+            r = random.randint(0,255)
+            g = random.randint(0,255)
+            b = random.randint(0,255)
+            draw_pixel(i, u, get_color(r, g, b))
 
 
 import tkinter as tk
-import random
 
 racine = tk.Tk() # Création de la fenêtre racine
 racine.title("dessin") # titre
 
-carre_noir = tk.Canvas(racine, bg="black", height=256, width= 256) # création widget
-
-bouton2 = tk.Button(racine, text="aleatoire", font = ("helvetica", "10"))
+carre_noir = tk.Canvas(racine, bg= "black", height=256, width= 256) # création widget
+draw_pixel(i, j, get_color(r, g, b))
+bouton2 = tk.Button(racine, text="aleatoire", font = ("helvetica", "10"), command = ecran_aleatoire)
 bouton3 = tk.Button(racine, text="degrade gris", font = ("helvetica", "10") )
 bouton4 = tk.Button(racine, text="degrade 2D", font = ("helvetica", "10") )
 
